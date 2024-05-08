ff4edad58393f2078a5b46d7d6a05ec9ad050f15
Diego Alleyne <714289@student.dusd.net>
2024-05-08 11:41:27 -0700

    UI Title Screen
    Made a UI for the title screen

--- P3UserInterfaceAlleyne/Assets/Course Library/Scripts/GameManager.cs
+++ P3UserInterfaceAlleyne/Assets/Course Library/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,14 +13,22 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    public void StartGame()
+    {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
-        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -46,6 +56,12 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
